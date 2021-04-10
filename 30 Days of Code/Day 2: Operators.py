ee3fac937cf2603aa98e92d20daee9086ba32fae
Avinash Prasad <avinashprasad1304@gmail.com>
2021-04-11 01:34:16 +0530

    Create Day 2: Operators
    Practice > Tutorials > 30 Days of Code > Day 2: Operators

---
+++ 30 Days of Code/Day 2: Operators.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = (meal_cost/100)*tip_percent
+    tax = (meal_cost/100)*tax_percent
+    total_cost = meal_cost+tip+tax
+    return round(total_cost)
+
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    total_cost = solve(meal_cost, tip_percent, tax_percent)
+    print(total_cost)
